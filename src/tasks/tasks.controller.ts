ce258ebd3d1b40d3664657d4a18c20e2817fad4f
furkan.turgut@turkuvazinovasyon.com <furkan.turgut@turkuvazinovasyon.com>
2024-08-17 17:35:23 +0300

    first commit task-manager

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,23 @@
+/* eslint-disable prettier/prettier */
+//nest g controller task
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { Task } from './task.model';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private taskService: TasksService) {}
+
+  @Get()
+  getAllTasks(): Task[] {
+    return this.taskService.getAllTasks();
+  }
+
+  @Post()
+  createTask(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Task {
+    return this.taskService.createTask(title, description);
+  }
+}
---
+++ src/tasks/tasks.module.ts
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+// nest g module tasks
+import { Module } from '@nestjs/common';
+import { TasksController } from './tasks.controller';
+import { TasksService } from './tasks.service';
+
+@Module({
+  controllers: [TasksController],
+  providers: [TasksService],
+})
+export class TasksModule {}
